2c9a5dab153255e8bfeb30c77102a8e75eda5381
Angel Xex <darkxex@gmail.com>
2022-04-22 16:08:14 -0400

    new update

--- 4KNativeEditor/Form1.cs
+++ 4KNativeEditor/Form1.cs
@@ -47,7 +47,7 @@ namespace _4KNativeEditor
            
             using (FileStream sr = File.OpenWrite(@"GameAssembly.dll"))
             {
-                sr.Seek(0x9C152A, SeekOrigin.Begin);
+                sr.Seek(0x9ED17A, SeekOrigin.Begin);
                 sr.WriteByte((byte)Width);
        
                
